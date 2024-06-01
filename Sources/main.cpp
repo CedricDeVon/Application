b38af7fdeb15569382fb905173a0eff9edcb710e
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-06-01 13:46:14 +0800

    Continued setup

---
+++ Sources/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    std::cout << "Hello World!" << '\n';
+
+    int exitCode = 0;
+    std::cin >> exitCode;
+    return 0;
+}
