0e272f5aa4a6bc6cb87b936ce9f9428cd92793ff
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-06-07 21:53:16 +0800

    Refactored code

--- Sources/2024/Demo/main.cpp
+++ Sources/2024/Demo/main.cpp
@@ -7,16 +7,16 @@
 
 int main(void)
 {
-    // std::ios_base::sync_with_stdio(false);
-    // std::cin.tie(NULL);
-    // std::cout.tie(NULL);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
 
-    std::cout << "(25 & 12) : " << (25 & 12) << '\n';
-    std::cout << "(25 | 12) : " << (25 | 12) << '\n';
-    std::cout << "(25 ^ 12) " << (25 ^ 12) << '\n';
-    std::cout << "~(35) : " << ~(35) << '\n';
-    std::cout << "(212 >> n) : " << (212 >> 0) << ", " << (212 >> 1) << ", " << (212 >> 2) << '\n';
-    std::cout << "(212 << n) : " << (212 << 0) << ", " << (212 << 1) << ", " << (212 << 2) << '\n';
+    // std::cout << "(25 & 12) : " << (25 & 12) << '\n';
+    // std::cout << "(25 | 12) : " << (25 | 12) << '\n';
+    // std::cout << "(25 ^ 12) " << (25 ^ 12) << '\n';
+    // std::cout << "~(35) : " << ~(35) << '\n';
+    // std::cout << "(212 >> n) : " << (212 >> 0) << ", " << (212 >> 1) << ", " << (212 >> 2) << '\n';
+    // std::cout << "(212 << n) : " << (212 << 0) << ", " << (212 << 1) << ", " << (212 << 2) << '\n';
 
     bool exitCode;
     std::cin >> exitCode;
