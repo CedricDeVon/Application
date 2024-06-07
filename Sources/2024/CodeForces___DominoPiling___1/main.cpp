a3027bb16f49066515f55260a3a67fff7b715f43
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2024-06-07 22:14:33 +0800

    Solved the 'Domino Piling' problem in codeforces.com

---
+++ Sources/2024/CodeForces___DominoPiling___1/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << (a * b / 2) << '\n';
+
+    // int exitCode;
+    // std::cin >> exitCode;
+    return 0;
+}
